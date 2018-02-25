193ad21b8df479a7d77001b659b13d93b71a2c4e
alex <alex@activewidgets.com>
2018-02-25 20:52:32 +0100

    update examples

--- examples/basic.js
+++ examples/basic.js
@@ -1,22 +1,7 @@
 
 import Vue from 'vue';
 import {Grid} from '../';
-import rows from './data/customers.js';
-
-
-let columns = [
-    {key: 'CustomerID', header: 'Code', width: 80, position: 'left', background: '#def'},
-    {key: 'CompanyName', header: 'Company Name', width: 150, position: 'left'},
-    {key: 'ContactName', header: 'Contact', width: 120},
-    {key: 'ContactTitle', header: 'Title', width: 120, background: '#fed'},
-    {key: 'Address', header: 'Address', width: 120},
-    {key: 'City', header: 'City', width: 80},
-    {key: 'PostalCode', header: 'Zip', width: 80, align: 'right'},
-    {key: 'Phone', header: 'Phone', width: 100},
-    {key: 'Fax', header: 'Fax', width: 100},
-    {key: 'Country', header: 'Country', width: 80, position: 'right', background: '#efe'}
-];
-
+import {columns, rows} from './data/customers.js';
 
 let components= {Grid};
 
