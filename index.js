43714e6c6175c5d143e5a9756223dc14b4978f0d
alex <alex@activewidgets.com>
2019-05-06 23:30:00 +0200

    use build()

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 
 import Vue from 'vue';
 import framework from '@activewidgets/frameworks/vue';
-import {grid, row} from '@activewidgets/grid';
+import * as pkg from '@activewidgets/grid';
 
-let {component} = framework(Vue);
+let {build} = framework(Vue);
 
-export const Grid = component(grid);
-export const Row = component(row);
-
-export const components = {Grid, Row};
+export const components = build(pkg);
+export const {Grid, Row, View, Scrollbars} = components;
 
 export function install(Vue){
     for(let i in components){
