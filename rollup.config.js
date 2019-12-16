6d44ec6d9c25611b7cfbe8a3247cff0e427ccc9c
alex <alex@activewidgets.com>
2019-12-16 20:33:00 +0100

    build

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,8 @@ import resolve from '@rollup/plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 import postcss from 'rollup-plugin-postcss';
 import sourcemaps from 'rollup-plugin-sourcemaps';
+import fs from 'fs';
+import path from 'path';
 
 
 let name = 'ActiveWidgets.Vue',
@@ -16,6 +18,12 @@ let globals = {
     '@activewidgets/datagrid/style': 'ActiveWidgets.styles',
     '@activewidgets/datagrid/metadata': 'ActiveWidgets.metadata',
     '@activewidgets/datagrid/css': ''
+};
+
+
+let roots = {
+    modules: ['', 'bundle', 'css', 'js'],
+    bundles: ['', 'bundle']
 };
 
 
@@ -34,29 +42,49 @@ let plugins = [
 ];
 
 
-let external = Object.keys(globals),
-    compact = true,
-    sourcemap = true,
-    sourcemapPathTransform = s => s.replace('node_modules', '../..');
+function read(filename){
+    return String(fs.readFileSync(filename, {encoding:'utf8'}));
+}
 
 
-let modules = file => [
-    {format: 'umd', sourcemap, globals, name, compact, file: 'dist/ax-' + file + '.umd.js'},
-    {format: 'esm', sourcemap, file: 'dist/ax-' + file + '.esm.js'}
-];
+let modules = roots.modules.map(dir => {
+
+    let pkg = JSON.parse(read(path.join(dir, 'package.json'))),
+        input = path.join(dir, 'index.js'),
+        main = path.join(dir, pkg.main),
+        module = path.join(dir, pkg.module),
+        sourcemap = true,
+        compact = true;
+
+    return {
+        input,
+        output: [
+            {file: main, format: 'umd', sourcemap, globals, name, compact},
+            {file: module, format: 'esm', sourcemap},
+        ],
+        external: Object.keys(globals),
+        plugins
+    };
+});
 
 
-let browser = file => [
-    {format: 'umd', sourcemap, sourcemapPathTransform, globals, name, compact, file: 'dist/' + file + '.js'}
-];
+let bundles = roots.bundles.map(dir => {
+
+    let input = path.join(dir, 'index.js'),
+        file = path.join('dist', (dir || 'ax') + '.js'),
+        sourcemapPathTransform = s => s.replace('node_modules', '../..'),
+        sourcemap = true,
+        compact = true;
+
+    return {
+        input,
+        output: [
+            {file, format: 'umd', sourcemap, sourcemapPathTransform, globals, name, compact}
+        ],
+        external: framework,
+        plugins
+    };
+});
 
 
-export default [
-    {input: 'index.js', output: modules('package'), external, plugins},
-    {input: 'js/index.js', output: modules('js'), external, plugins},
-    {input: 'css/index.js', output: modules('css'), external, plugins},
-    {input: 'bundle/index.js', output: modules('bundle'), external, plugins},
-
-    {input: 'index.js', output: browser('ax'), external: framework, plugins},
-    {input: 'bundle/index.js', output: browser('bundle'), external: framework, plugins}
-];
+export default [].concat(modules, bundles);
