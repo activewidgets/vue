e7bdd788f3662cf58e86e66924ebd456b4072d71
alex <alex@activewidgets.com>
2019-12-23 00:03:14 +0100

    add data samples

--- examples/viewer.js
+++ examples/viewer.js
@@ -2,9 +2,12 @@
 import Vue from "vue";
 import * as components from '../';
 import * as pages from './index.js';
+import * as northwind from './northwind.js';
 
 
 let el = document.querySelector('#app>div'),
+    templates = {},
+    data = {northwind},
     lookup = {},
     links = [],
     key = 1,
@@ -60,7 +63,7 @@ function refresh(){
         mode = RegExp.$1;
 
     if (name && typeof lookup[name] == 'function') {
-        lookup[name]({render}, components, {});
+        lookup[name]({render}, components, templates, data);
     }
     else {
         index(name);
