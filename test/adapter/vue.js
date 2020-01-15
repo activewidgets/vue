abeff4f14524c14a93ea956b66d30cb68e094736
alex <alex@activewidgets.com>
2020-01-15 17:36:46 +0100

    fix event handlers in test adapter

--- test/adapter/vue.js
+++ test/adapter/vue.js
@@ -3,8 +3,20 @@ import {render as originalRender, fireEvent, wait, waitForElement} from '@testin
 
 
 export function render(component, props){
+
+    let i, attrs = {}, on = {};
+
+    for (i in props){
+        if (i.slice(0, 2) == 'on'){
+            on[i.slice(2).toLowerCase()] = props[i];
+        }
+        else {
+            attrs[i] = props[i];
+        }
+    }
+
     return originalRender({
-        render: h => h(component, {attrs: props})
+        render: h => h(component, {attrs, on})
     });
 }
 
