f9bd5bc27e06778aebe33d6ce2d82e73d9fbc9a0
alex <alex@activewidgets.com>
2020-01-30 21:27:01 +0100

    hello world

---
+++ examples/hello-world/src/app.vue
@@ -0,0 +1,22 @@
+
+<template>
+    <ax-datagrid :rows="rows"></ax-datagrid>
+</template>
+<script>
+
+import {components} from '@activewidgets/vue';
+import './styles.css';
+
+
+function data(){
+    return {
+        rows: [
+            { message: 'Hello, World!' }
+        ]
+    };
+}
+
+
+export default { components, data };
+
+</script>
--- examples/viewer/examples.js
+++ examples/viewer/examples.js
@@ -1,3 +1,11 @@
 ﻿
+import hello_world from '../hello-world/README.md';
+
+
+export const Local = {
+    'Hello, World!': {path: 'hello-world', readme: hello_world}
+};
+
+
 export * from '@activewidgets/datagrid/examples';
 
