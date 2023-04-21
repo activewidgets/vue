98b64a55ea9bfd244946079b44ad93e2a4dded83
alex <alex@activewidgets.com>
2023-04-21 20:02:03 +0200

    switch to type: module

--- rollup.config.js
+++ rollup.config.js
@@ -17,7 +17,6 @@ let globals = {
     '@activewidgets/frameworks/vue': 'ActiveWidgets.Frameworks.Vue',
     '@activewidgets/datagrid/js': 'ActiveWidgets.Components',
     '@activewidgets/datagrid/style': 'ActiveWidgets.Styles',
-    '@activewidgets/datagrid/dist/datagrid.css': '',
     '@activewidgets/datagrid/css': ''
 };
 
